4d70df74195ee163e06b15927016465e38c612e0
1must <1079140193@qq.com>
2019-07-23 14:35:40 +0800

    tag组件更新

--- src/pages/components/tag/tag.js
+++ src/pages/components/tag/tag.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import './tag.scss'
-export default class Tag extends React.Component{
-    //props:{
-    //          value
-    //          color
-    constructor(props){
-        super(props)
-    }
-
-    render(){
-        return (
-            <div className='tag' style={{'backgroundColor':this.props.color||'green'}}>
-                {this.props.value}
-            </div>
-        )
-    }
+export default function Tag(props){
+    return (
+        <div className='tag' style={{'backgroundColor':props.color||'green'}}>
+            {props.value}
+        </div>
+    )
 }
